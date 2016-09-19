4b801f0a0f05dbf06ed7d15cd7fff41c93530435
james727 <jameshkatz@gmail.com>
2016-09-18 23:06:49 -0400

    Updated readme and added url_score logic

--- readme.md
+++ readme.md
@@ -15,3 +15,22 @@ It will prompt using drop-down menus as well, as seen below:
 After a few questions, it will spit out a rough approximation of a business' creditworthiness on a scale of 0-1:
 
 ![final output](http://i.imgur.com/0PRT1rb.png)
+
+##How it works
+The program works by taking a weighted average of 5 factors that can be indicators of good credit:
+
+1. **Company Yelp score** - a score based on the number and quality of Yelp reviews a company receives
+2. **Industry score** - a score based on the volatility of the company's industry
+3. **Size score** - a score based on the size of the company
+4. **Owner score** - a score based on the creditworthiness of the company owner (just a placeholder for now)
+5. **URL score** - a score based on the trustworthiness of the company's website (just a placeholder for now)
+
+For more details as to how these scores are calculated, see the relevant source code (there's a separate file for each scoring procedure).
+
+##Navigating the source
+The two most important files are the following:
+
+1. **user_inputs.py** - contains the scripts for prompting and validating all user input.
+2. **score_aggregator.py** - calls each of the individual scoring routines and aggregates them into one score.
+
+The file **main.py** simply calls **user_inputs.py**, which gathers all data about the company and returns a company object (the company class can be found in the **company_info.py** file). Then, **score_aggregator.py** is used to calculate and print a score for the company.
--- score_aggregator.py
+++ score_aggregator.py
@@ -4,6 +4,7 @@ COMPANY_WEIGHT = 1
 INDUSTRY_WEIGHT = 1
 SIZE_WEIGHT = 1
 OWNER_WEIGHT = 1
+URL_WEIGHT = 1
 
 def aggregate_scores(company):
     # Takes a company object and tabulates all relevant scores. Then calls combine_scores
@@ -14,15 +15,17 @@ def aggregate_scores(company):
     company_industry_score = sector_score.get_industry_score(company)
     company_size_score = size_score.get_size_score(company)
     company_owner_score = owner_score.get_owner_score(company)
-    return combine_scores(company_score, company_industry_score, company_size_score, company_owner_score)
+    company_url_score = url_score.get_url_score(company)
+    return combine_scores(company_score, company_industry_score, company_size_score, company_owner_score, company_url_score)
 
-def combine_scores(company_score, industry_score, size_score, owner_score):
+def combine_scores(company_score, industry_score, size_score, owner_score, link_score):
     # Produces a weighted average score based on the weightings assigned up top.
     company_factor = COMPANY_WEIGHT * company_score
     industry_factor = INDUSTRY_WEIGHT * industry_score
     size_factor = SIZE_WEIGHT * size_score
     owner_factor = OWNER_WEIGHT * owner_score
+    url_factor = URL_WEIGHT * link_score
 
-    total_weight = COMPANY_WEIGHT + INDUSTRY_WEIGHT + SIZE_WEIGHT + OWNER_WEIGHT
-    weighted_score = (company_factor + industry_factor + size_factor + owner_factor + 0.0) / total_weight
+    total_weight = COMPANY_WEIGHT + INDUSTRY_WEIGHT + SIZE_WEIGHT + OWNER_WEIGHT + URL_WEIGHT
+    weighted_score = (company_factor + industry_factor + size_factor + owner_factor + url_factor + 0.0) / total_weight
     return weighted_score
